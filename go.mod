92a07555c9950702745ac4db90746b2b5279152f
Jeffrey Stoke <js@datatroniq.com>
2020-08-13 12:27:09 +0200

    Add kubehelper for client wrapup

--- go.mod
+++ go.mod
@@ -18,6 +18,35 @@ require (
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
 	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
 	google.golang.org/grpc v1.27.1
-	k8s.io/apimachinery v0.18.5
-	k8s.io/client-go v0.18.5
+	k8s.io/api v0.18.6
+	k8s.io/apimachinery v0.18.6
+	k8s.io/client-go v0.18.6
+	k8s.io/kubernetes v1.18.6
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.18.6
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.18.6
+	k8s.io/apimachinery => k8s.io/apimachinery v0.18.6
+	k8s.io/apiserver => k8s.io/apiserver v0.18.6
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.18.6
+	k8s.io/client-go => k8s.io/client-go v0.18.6
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.18.6
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.18.6
+	k8s.io/code-generator => k8s.io/code-generator v0.18.6
+	k8s.io/component-base => k8s.io/component-base v0.18.6
+	k8s.io/controller-manager => k8s.io/controller-manager v0.18.6
+	k8s.io/cri-api => k8s.io/cri-api v0.18.6
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.18.6
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.18.6
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.18.6
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.18.6
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.18.6
+	k8s.io/kubectl => k8s.io/kubectl v0.18.6
+	k8s.io/kubelet => k8s.io/kubelet v0.18.6
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.18.6
+	k8s.io/metrics => k8s.io/metrics v0.18.6
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.18.6
+	k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.18.6
+	k8s.io/sample-controller => k8s.io/sample-controller v0.18.6
 )
---
+++ kubehelper/storage.go
@@ -0,0 +1,275 @@
+package kubehelper
+
+import (
+	"context"
+	"fmt"
+
+	storageapiv1 "k8s.io/api/storage/v1"
+	storageapiv1b1 "k8s.io/api/storage/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/conversion"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/storage/v1"
+	"k8s.io/client-go/kubernetes/typed/storage/v1beta1"
+	"k8s.io/kubernetes/pkg/apis/storage"
+	storagev1 "k8s.io/kubernetes/pkg/apis/storage/v1"
+	storagev1b1 "k8s.io/kubernetes/pkg/apis/storage/v1beta1"
+)
+
+func CreateCSIDriverClient(apiResources []*metav1.APIResourceList, kubeClient kubernetes.Interface) *CSIDriverClient {
+	client := &CSIDriverClient{}
+
+	discovery.FilteredBy(discovery.ResourcePredicateFunc(func(groupVersion string, r *metav1.APIResource) bool {
+		gvk := metav1.GroupVersionKind{
+			Group:   r.Group,
+			Version: r.Version,
+			Kind:    r.Kind,
+		}
+
+		switch gvk.String() {
+		case storagev1.SchemeGroupVersion.WithKind("CSIDriver").String():
+			client.V1Client = kubeClient.StorageV1().CSIDrivers()
+		case storagev1b1.SchemeGroupVersion.WithKind("CSIDriver").String():
+			client.V1b1Client = kubeClient.StorageV1beta1().CSIDrivers()
+		}
+
+		return false
+	}), apiResources)
+
+	return client
+}
+
+type CSIDriverClient struct {
+	V1Client   v1.CSIDriverInterface
+	V1b1Client v1beta1.CSIDriverInterface
+}
+
+func (c *CSIDriverClient) Create(ctx context.Context, csiDriver *storage.CSIDriver, opts metav1.CreateOptions) (*storage.CSIDriver, error) {
+	var (
+		err    error
+		result = new(storage.CSIDriver)
+	)
+
+	switch {
+	case c.V1Client != nil:
+		out := new(storageapiv1.CSIDriver)
+		// scope is not used
+		err = storagev1.Convert_storage_CSIDriver_To_v1_CSIDriver(csiDriver, out, conversion.Scope(nil))
+		if err != nil {
+			return nil, err
+		}
+
+		out, err = c.V1Client.Create(ctx, out, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1.Convert_v1_CSIDriver_To_storage_CSIDriver(out, result, conversion.Scope(nil))
+	case c.V1b1Client != nil:
+		out := new(storageapiv1b1.CSIDriver)
+		// scope is not used
+		err = storagev1b1.Convert_storage_CSIDriver_To_v1beta1_CSIDriver(csiDriver, out, conversion.Scope(nil))
+		if err != nil {
+			return nil, err
+		}
+
+		out, err = c.V1b1Client.Create(ctx, out, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1b1.Convert_v1beta1_CSIDriver_To_storage_CSIDriver(out, result, conversion.Scope(nil))
+	default:
+		return nil, fmt.Errorf("unsupported CSIDriver api version")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *CSIDriverClient) Update(ctx context.Context, csiDriver *storage.CSIDriver, opts metav1.UpdateOptions) (*storage.CSIDriver, error) {
+	var (
+		err    error
+		result = new(storage.CSIDriver)
+	)
+
+	switch {
+	case c.V1Client != nil:
+		out := new(storageapiv1.CSIDriver)
+		// scope is not used
+		err = storagev1.Convert_storage_CSIDriver_To_v1_CSIDriver(csiDriver, out, conversion.Scope(nil))
+		if err != nil {
+			return nil, err
+		}
+
+		out, err = c.V1Client.Update(ctx, out, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1.Convert_v1_CSIDriver_To_storage_CSIDriver(out, result, conversion.Scope(nil))
+	case c.V1b1Client != nil:
+		out := new(storageapiv1b1.CSIDriver)
+		// scope is not used
+		err = storagev1b1.Convert_storage_CSIDriver_To_v1beta1_CSIDriver(csiDriver, out, conversion.Scope(nil))
+		if err != nil {
+			return nil, err
+		}
+
+		out, err = c.V1b1Client.Update(ctx, out, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1b1.Convert_v1beta1_CSIDriver_To_storage_CSIDriver(out, result, conversion.Scope(nil))
+	default:
+		return nil, fmt.Errorf("unsupported CSIDriver api version")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *CSIDriverClient) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
+	switch {
+	case c.V1Client != nil:
+		return c.V1Client.Delete(ctx, name, opts)
+	case c.V1b1Client != nil:
+		return c.V1b1Client.Delete(ctx, name, opts)
+	default:
+		return fmt.Errorf("unsupported CSIDriver api version")
+	}
+}
+
+func (c *CSIDriverClient) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listOpts metav1.ListOptions) error {
+	switch {
+	case c.V1Client != nil:
+		return c.V1Client.DeleteCollection(ctx, opts, listOpts)
+	case c.V1b1Client != nil:
+		return c.V1b1Client.DeleteCollection(ctx, opts, listOpts)
+	default:
+		return fmt.Errorf("unsupported CSIDriver api version")
+	}
+}
+
+func (c *CSIDriverClient) Get(ctx context.Context, name string, opts metav1.GetOptions) (*storage.CSIDriver, error) {
+	var (
+		err    error
+		result = new(storage.CSIDriver)
+	)
+
+	switch {
+	case c.V1Client != nil:
+		var ret *storageapiv1.CSIDriver
+		ret, err = c.V1Client.Get(ctx, name, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1.Convert_v1_CSIDriver_To_storage_CSIDriver(ret, result, conversion.Scope(nil))
+	case c.V1b1Client != nil:
+		var ret *storageapiv1b1.CSIDriver
+		ret, err = c.V1b1Client.Get(ctx, name, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1b1.Convert_v1beta1_CSIDriver_To_storage_CSIDriver(ret, result, conversion.Scope(nil))
+	default:
+		return nil, fmt.Errorf("unsupported CSIDriver api version")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *CSIDriverClient) List(ctx context.Context, opts metav1.ListOptions) (*storage.CSIDriverList, error) {
+	var (
+		err    error
+		result = new(storage.CSIDriverList)
+	)
+
+	switch {
+	case c.V1Client != nil:
+		var ret *storageapiv1.CSIDriverList
+		ret, err = c.V1Client.List(ctx, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1.Convert_v1_CSIDriverList_To_storage_CSIDriverList(ret, result, conversion.Scope(nil))
+	case c.V1b1Client != nil:
+		var ret *storageapiv1b1.CSIDriverList
+		ret, err = c.V1b1Client.List(ctx, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1b1.Convert_v1beta1_CSIDriverList_To_storage_CSIDriverList(ret, result, conversion.Scope(nil))
+	default:
+		return nil, fmt.Errorf("unsupported CSIDriver api version")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *CSIDriverClient) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
+	switch {
+	case c.V1Client != nil:
+		return c.V1Client.Watch(ctx, opts)
+	case c.V1b1Client != nil:
+		return c.V1b1Client.Watch(ctx, opts)
+	default:
+		return nil, fmt.Errorf("unsupported CSIDriver api version")
+	}
+}
+
+func (c *CSIDriverClient) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subResources ...string) (*storage.CSIDriver, error) {
+	var (
+		err    error
+		result = new(storage.CSIDriver)
+	)
+
+	switch {
+	case c.V1Client != nil:
+		var ret *storageapiv1.CSIDriver
+		ret, err = c.V1Client.Patch(ctx, name, pt, data, opts, subResources...)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1.Convert_v1_CSIDriver_To_storage_CSIDriver(ret, result, conversion.Scope(nil))
+	case c.V1b1Client != nil:
+		var ret *storageapiv1b1.CSIDriver
+		ret, err = c.V1b1Client.Patch(ctx, name, pt, data, opts, subResources...)
+		if err != nil {
+			return nil, err
+		}
+
+		err = storagev1b1.Convert_v1beta1_CSIDriver_To_storage_CSIDriver(ret, result, conversion.Scope(nil))
+	default:
+		return nil, fmt.Errorf("unsupported CSIDriver api version")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
